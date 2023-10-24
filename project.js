b5e26ef148992734569c97c4d3c595023a914daa
a21rublormar <a21rublormar@inspedralbes.cat>
2023-10-24 13:17:53 +0200

    actualizado index, project.js and copManager

--- project.js
+++ project.js
@@ -1,6 +1,7 @@
 import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
 import { getProductes } from '/copManager.js';
 
+
 createApp({
     data() {
         return {
@@ -13,13 +14,15 @@ createApp({
     computed: {
         filteredProducts(){
 
+
             return this.productes.productes.filter((disc) =>
-                disc.nombre.toLowerCase().includes(this.search.toLowerCase()) || 
+                disc.nombre.toLowerCase().includes(this.search.toLowerCase()) ||
                 disc.artista.toLowerCase().includes(this.search.toLowerCase()) ||
                 disc.genero.toLowerCase().includes(this.search.toLowerCase())
             );
         },
     },
+
 
     methods: {
         mostrar(div) {
