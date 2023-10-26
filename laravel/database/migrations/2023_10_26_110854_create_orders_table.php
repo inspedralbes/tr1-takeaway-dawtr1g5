75aa141e79f5419c1f5ba52bbc27d6994e1644c7
a21rublormar <a21rublormar@inspedralbes.cat>
2023-10-26 13:26:34 +0200

    Controller: oderController, Model: Order artisan create!

--- laravel/database/migrations/2023_10_26_110854_create_orders_table.php
+++ laravel/database/migrations/2023_10_26_110854_create_orders_table.php
@@ -4,17 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('tickets', function (Blueprint $table) {
-            $table->id('idComanda');
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
             $table->decimal('final_price', 10, 2);
             $table->enum('estat', ['Pendent de preparar', 'En preparació', 'Preparat per recollir'])->default('Pendent de preparar');
-            $table->binary('products');
+            
             $table->timestamps();
         });
     }
@@ -24,6 +25,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('tickets');
+        Schema::dropIfExists('orders');
     }
 };
