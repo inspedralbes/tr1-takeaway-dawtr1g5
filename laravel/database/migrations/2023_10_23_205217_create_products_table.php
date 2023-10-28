15336d6c60cf1d007faa4834d471f24e979302cb
a22rauespgom <a22rauespgom@inspedralbes.cat>
2023-10-28 11:48:02 +0200

    Getting ruben2 laravel branch

--- laravel/database/migrations/2023_10_23_205217_create_products_table.php
+++ laravel/database/migrations/2023_10_23_205217_create_products_table.php
@@ -16,7 +16,6 @@ return new class extends Migration {
             $table->string('artist');
             $table->string('year');
             $table->decimal('price', 8, 2);
-            $table->integer('count')->default(1);
             $table->binary('image')->nullable();
             $table->timestamps();
         });
