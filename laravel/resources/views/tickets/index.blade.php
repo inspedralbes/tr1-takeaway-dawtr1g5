4780efd7397f6ca232b8fcdbdcfb03c2b35644d9
a22rauespgom <a22rauespgom@inspedralbes.cat>
2023-10-30 16:00:55 +0100

    Merging x2

--- laravel/resources/views/tickets/index.blade.php
+++ laravel/resources/views/tickets/index.blade.php
@@ -1,20 +1,5 @@
 @extends('app')
 @section('content')
-<div class="">
-    <h1>Llistat de tickets</h1>
-    <hr>
-    <div class="">
-        @if (session('success'))
-        <div class="message is-success">
-            <h6>{{ session('success') }}</h6>
-        </div>
-        @endif
-        <ul>
-            @foreach ($tickets as $ticket)
-            <li><a href="{{ route('tickets-edit', ['id' => $ticket->id]) }}"> ticket {{ $ticket->id}} -> {{
-                    $ticket->final_price}} | {{$ticket->estat}}</a></li>
-            @endforeach
-        </ul>
-    </div>
-</div>
+<h1>Hola</h1>
 @endsection
+
