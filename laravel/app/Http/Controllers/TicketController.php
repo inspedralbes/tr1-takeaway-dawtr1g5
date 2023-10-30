8efb2223d9b7cf5f0c21ad8898d686fbc74f7d69
a22rauespgom <a22rauespgom@inspedralbes.cat>
2023-10-30 16:01:23 +0100

    Revert "merging Ruben branch into develop"
    This reverts commit 6b2702501ac9675fcea67b176f3dc623a00c960a.

--- laravel/app/Http/Controllers/TicketController.php
+++ laravel/app/Http/Controllers/TicketController.php
@@ -9,14 +9,12 @@ use Illuminate\Support\Facades\DB;
 
 class TicketController extends Controller
 {
-    //MOSTRA TOTS ELS TICKETS EN UN LLISTAT
-
-
+    //
     public function index_all()
     {
         $ticket = ticket::all();
 
-        return view('tickets.index', ['tickets' => $ticket]);
+        return response()->json($ticket);
     }
 
 
@@ -24,6 +22,7 @@ class TicketController extends Controller
     {
         ///GET POST REQUEST DATA
         $data = $request->all();
+
         ///STORE TICKET DATA
         $ticket = new Ticket;
         $ticket->final_price = $data["precio"];
@@ -56,29 +55,40 @@ class TicketController extends Controller
             ->select('tickets.*', 'linea_tickets.*')
             ->where('tickets.id', '=', $id)
             ->get();
-        $linea_ticket = LineaTicket::all();
+
+        return response()->json($ticket);
+    }
+
+    public function getLastTicket()
+    {
+        $ticket = DB::table('tickets')
+            ->join('linea_tickets', 'linea_tickets.ticket_id', '=', 'tickets.id')
+            ->select('tickets.*', 'linea_tickets.*')
+            ->latest('tickets.id') // Ordenar por el campo id de forma descendente
+            ->first();
+
+        return response()->json($ticket);
+    }
+
+    public function showWeb($id)
+    {
+        $ticket = DB::table('tickets')
+            ->join('linea_tickets', 'linea_tickets.ticket_id', '=', 'tickets.id')
+            ->select('tickets.*', 'linea_tickets.*')
+            ->where('tickets.id', '=', $id)
+            ->get();
 
         // dd($ticket);
-        // dd($linea_ticket);
 
-        return view('tickets.show', ['tickets' => $ticket, 'linea_ticket' => $linea_ticket]);
+        return view('tickets.show', ['ticket' => $ticket]);
     }
-    public function update(Request $request, $id)
+    public function update($id)
     {
-
-        $ticket = ticket::find($id);
-        $ticket->estat = $request->estat;
-
-        $ticket->save();
-
-        return redirect()->route('tickets')->with('success', 'Ticket actualitzat correctament!');
     }
 
     public function destroy($id)
     {
         $ticket = ticket::find($id);
         $ticket->delete();
-
-        return redirect()->route('tickets')->with('success', 'El Ticket ha sigut eliminat correctament!');
     }
 }
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\productsController;
 use App\Http\Controllers\TicketController;
+use PHPUnit\Framework\Attributes\Ticket;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,29 +20,18 @@ use App\Http\Controllers\TicketController;
 Route::get('/', function () {
     return view('products.index');
 });
-Route::get('/tickets', function () {
-    return view('tickets.index');
+Route::get('/ticket', function () {
+    return view('tickets.show');
 });
 
-Route::get('/app', function () {
-    return view('app');
-});
-
-// Route::get('/comand', function(){
-//     return view('products.comandes');
-// });
-
-
-//PRODUCTS
 Route::get('/', [productsController::class, 'index_all'])->name('products');
 Route::post('/', [productsController::class, 'store'])->name('products');
 Route::get('/products/{id}', [productsController::class, 'show'])->name('products-edit');
 Route::patch('/products/{id}', [productsController::class, 'update'])->name('products-update');
+
 Route::delete('/products/{id}', [productsController::class, 'destroy'])->name('products-destroy');
 
-//TICKETS
+Route::post('/', [TicketController::class, 'store'])->name('tickets');
 Route::get('/tickets', [TicketController::class, 'index_all'])->name('tickets');
-Route::post('/tickets', [TicketController::class, 'store'])->name('tickets');
-Route::get('/tickets/{id}', [TicketController::class, 'show'])->name('tickets-edit');
-Route::patch('/ticket/{id}', [TicketController::class, 'update'])->name('tickets-update');
-Route::delete('/ticket/{id}', [TicketController::class, 'destroy'])->name('tickets-destroy');
+Route::get('/ticket/{id}', [TicketController::class, 'showWeb'])->name('tickets-update');
+
