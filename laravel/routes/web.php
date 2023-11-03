fbe6c76139e0789574c77fefb609a60f644e8a52
Fabianroldan <a21fabrolfer@inspedralbes.cat>
2023-11-03 13:07:30 +0100

    register y login funciona (no guarda bd)

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -19,30 +19,36 @@ use App\Http\Controllers\UserController;
 Route::get('/', function () {
     return view('products.index');
 });
+
 Route::get('/tickets', function () {
     return view('tickets.index');
 });
 
-Route::get('/register', [UserController::class, 'register']);
-Route::get('/login', [UserController::class, 'login']);
+Route::post('/register', function () {
+    return view('register.register');
+})->name('register');
+
+Route::post('/login', function () {
+    return view('register.login');
+})->name('login');
 
 Route::group(['middleware' => ["auth:sanctum"]], function() {
     Route::get('user-profile', [UserController::class, 'userProfile']);
     Route::get('logout', [UserController::class, 'logout']);
 
     //PRODUCTS
-    Route::get('/', [productsController::class, 'index_all'])->name('products');
-    Route::post('/', [productsController::class, 'store'])->name('products');
     Route::get('/products/{id}', [productsController::class, 'show'])->name('products-edit');
-    Route::patch('/products/{id}', [productsController::class, 'update'])->name('products-update');
-
+    Route::patch('/products/{id}', [productsController::class, 'update'])->name('products-update');   
     Route::delete('/products/{id}', [productsController::class, 'destroy'])->name('products-destroy');
-
+    
     //TICKETS
-    Route::get('/tickets', [TicketController::class, 'index_all'])->name('tickets');
-    Route::post('/tickets', [TicketController::class, 'store'])->name('tickets');
     Route::get('/tickets/{id}', [TicketController::class, 'show'])->name('tickets-edit');
     Route::patch('/ticket/{id}', [TicketController::class, 'update'])->name('tickets-update');
     Route::delete('/ticket/{id}', [TicketController::class, 'destroy'])->name('tickets-destroy');
-
+    
 });
+
+Route::get('/', [productsController::class, 'index_all'])->name('products');
+Route::post('/', [productsController::class, 'store'])->name('products');
+Route::get('/tickets', [TicketController::class, 'index_all'])->name('tickets');
+Route::post('/tickets', [TicketController::class, 'store'])->name('tickets');
