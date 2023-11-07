2596eb20b04bc5cedabf9ec64e6a4394f3f550b3
a21rublormar <a21rublormar@inspedralbes.cat>
2023-11-07 17:12:24 +0100

    intento de authentication login user with sanctum

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -6,6 +6,10 @@ use App\Http\Controllers\productsController;
 use App\Http\Controllers\TicketController;
 use App\Http\Controllers\genresController;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -36,3 +40,43 @@ Route::get('/ticketLast', [TicketController::class, 'getLastTicket']);
 
 ///GENRES
 Route::get('/genres', [genresController::class, 'index']);
+
+
+
+
+Route::post('/tokens/create', function (Request $request) {
+
+    $token = $request->user()->createToken($request->token_name);
+
+    return ['token' => $token->plainTextToken];
+
+});
+
+
+
+// //El middleware de capacidades se puede asignar a una ruta para verificar que el token de la solicitud entrante tenga todas las capacidades enumeradas
+// Route::get('/orders', function () {
+    
+// })->middleware(['auth:sanctum', 'abilities:check-status,place-orders']);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+    ]);
+ 
+    $user = User::where('email', $request->email)->first();
+ 
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+ 
+    return $user->createToken($request->device_name)->plainTextToken;
+});
